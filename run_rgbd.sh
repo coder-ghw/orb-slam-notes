f9153c94e9ae91517f94f118c50eeef2f461daf6
huwei.guo <huwei.guo@deptrum.com>
2023-06-05 13:47:35 +0800

    fetch

---
+++ run_rgbd.sh
@@ -0,0 +1,7 @@
+./Examples/RGB-D/rgbd_tum \
+  Vocabulary/ORBvoc.txt\
+  Examples/RGB-D/TUM1.yaml \
+  /home/guohw/data/DATA/TUM/rgbd_dataset_freiburg1_xyz\
+  /home/guohw/data/projects/orb_slam2/Examples/RGB-D/associations/fr1_xyz.txt
+
+#rosrun ORB_SLAM2 RGBD Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml
